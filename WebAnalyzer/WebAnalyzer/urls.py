ab2e6a988acfc78103dcccfff13307be6aaec592
hussainBurhan <144117030+hussainBurhan@users.noreply.github.com>
2023-12-19 23:08:15 +0500

    Comments added

--- WebAnalyzer/WebAnalyzer/urls.py
+++ WebAnalyzer/WebAnalyzer/urls.py
@@ -1,8 +1,8 @@
 """
 URL configuration for WebAnalyzer project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+The `urlpatterns` list routes URLs to views. For more information, please see:
+https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,12 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+# Import necessary modules
 from django.contrib import admin
 from django.urls import path
 from . import views
 
+# Define URL patterns for the WebAnalyzer app
 urlpatterns = [
+    # Admin URL for Django admin interface
     path('admin/', admin.site.urls),
+    
+    # URL for the home page, linked to the 'home' view function
     path('', views.home, name='home'),
+    
+    # URL for processing the form and displaying results, linked to the 'result' view function
     path('result', views.result, name='result'),
 ]
--- WebAnalyzer/WebAnalyzer/views.py
+++ WebAnalyzer/WebAnalyzer/views.py
@@ -1,25 +1,40 @@
+# Import necessary modules
 from django.shortcuts import render
 import operator
 import string
 
+# View for the home page
 def home(request):
+    # Render the home page template
     return render(request, 'index.html')
 
+# View for processing the input text and displaying results
 def result(request):
+    # Get the input text from the GET request parameters
     article = request.GET['article']
+
+    # Remove punctuation from the input text
     for punctuation in string.punctuation:
         article = article.replace(punctuation, '')
-    
+
+    # Split the input text into a list of words
     words = article.split()
+
+    # Calculate the total word count
     word_count = len(words)
 
+    # Initialize an empty dictionary to store word frequencies
     dict_words = {}
 
+    # Count the frequency of each word in the input text
     for word in words:
         if word in dict_words:
             dict_words[word] += 1
         else:
             dict_words[word] = 1
 
+    # Sort the dictionary by word frequency in descending order
     frequency = sorted(dict_words.items(), key=operator.itemgetter(1), reverse=True)
+
+    # Render the result page template with the relevant data
     return render(request, 'result.html', {'article': article, 'word_count': word_count, 'dict_words': dict_words, 'frequency': frequency})
